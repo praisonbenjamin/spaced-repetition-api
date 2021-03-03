47d64f2dd5b8ab3cb8ae65dd491d8cc5c54c5c22
Praison Benjamin <praisonbenjamin@gmail.com>
2021-03-03 12:27:54 -0600

    a

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,17 +1,9 @@
 require('dotenv').config();
-const pg = require('pg');
-pg.defaults.ssl = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'migrationDirectory': 'migrations',
+  'migrationsDirectory': 'migrations',
   'driver': 'pg',
-  'host': process.env.MIGRATION_DATABASE_HOST,
-  'port': process.env.MIGRATION_DATABASE_PORT,
-  'database': process.env.MIGRATION_DATABASE_NAME,
-  'username': process.env.MIGRATION_DATABASE_USER,
-  'password': process.env.MIGRATION_DATABASE_PASS,
-  connectionString:
-  process.env.NODE_ENV === 'test'
+  'connectionString': (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production',
